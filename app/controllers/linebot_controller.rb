332d583ab55ab643c2f2877361ed7075d6e62a68
Yusuke Miyazaki <ysk1180@gmail.com>
2018-06-22 15:46:14 +0900

    返答作成

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -22,19 +22,11 @@ class LinebotController < ApplicationController
       error 400 do 'Bad Request' end
     end
 
-    # 仮でここに記載
     url  = "http://www.drk7.jp/weather/xml/13.xml"
     xml  = open( url ).read.toutf8
     doc = REXML::Document.new(xml)
-    xpath = 'weatherforecast/pref/area[4]/info'
-    weather = doc.elements[xpath + '/weather'].text
-    per06to12 = doc.elements[xpath + '/rainfallchance/period[2]l'].text
-    per12to18 = doc.elements[xpath + '/rainfallchance/period[3]l'].text
-    per18to24 = doc.elements[xpath + '/rainfallchance/period[4]l'].text
+    xpath = 'weatherforecast/pref/area[4]/'
     min_per = 30
-    if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-      push = "今日は傘を持っていってね！#{weather}だよ"
-    end
 
     events = client.parse_events_from(body)
 
@@ -43,6 +35,57 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+          input = event.message['text']
+          case input
+          when /.*明日.*/
+            per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]l'].text
+            per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]l'].text
+            per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]l'].text
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              push =
+                "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+            else
+              push =
+                "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
+            end
+          when /.*明後日.*/
+            per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
+            per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
+            per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              push =
+                "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
+            else
+              push =
+                "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
+            end
+          when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|面白い|おもしろい).*/
+            push =
+              "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
+          when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
+            push =
+              "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+          else
+            per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
+            per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
+            per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]l'].text
+            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
+              word =
+                ["雨だけど元気出していこうね！",
+                 "雨に負けずファイト！！",
+                 "雨だけどああたの明るさでみんなを元気にしてあげて(^^)"].sample
+              push =
+                "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
+            else
+              word =
+                ["天気もいいから一駅歩いてみるのはどう？(^^)",
+                 "今日会う人のいいところを見つけて是非その人に教えてあげて(^^)",
+                 "素晴らしい一日になりますように(^^)",
+                 "雨が降っちゃったらごめんね(><)"].sample
+              push =
+                "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
+            end
+          end
           message = {
             type: 'text',
             text: push
