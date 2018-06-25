1876d82e2ef2dc199b2e2f065c0d5599840a0964
Yusuke Miyazaki <ysk1180@gmail.com>
2018-06-25 13:30:50 +0900

    リファクタリング・チュートリアル用のコメント追加

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -16,31 +16,33 @@ class LinebotController < ApplicationController
 
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
-
-    url  = "http://www.drk7.jp/weather/xml/13.xml"
-    xml  = open( url ).read.toutf8
-    doc = REXML::Document.new(xml)
-    xpath = 'weatherforecast/pref/area[4]/'
-    min_per = 30
-
     events = client.parse_events_from(body)
-
     events.each { |event|
       case event
+      # メッセージが送信された場合の対応
       when Line::Bot::Event::Message
         case event.type
+        # ユーザーからテキスト形式のメッセージが送られて来た場合
         when Line::Bot::Event::MessageType::Text
+          # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
+          url  = "http://www.drk7.jp/weather/xml/13.xml"
+          xml  = open( url ).read.toutf8
+          doc = REXML::Document.new(xml)
+          xpath = 'weatherforecast/pref/area[4]/'
+          # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
+          min_per = 30
           case input
+            # 「明日」or「あした」というワードが含まれる場合
           when /.*(明日|あした).*/
-            per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]l'].text
-            per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]l'].text
-            per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]l'].text
+            # info[2]：明日の天気
+            per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
+            per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
+            per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               push =
                 "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
@@ -86,6 +88,7 @@ class LinebotController < ApplicationController
                 "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
             end
           end
+        # テキスト以外（画像等）のメッセージが送られた場合
         else
           push = "テキスト以外はわからないよ〜(；；)"
         end
@@ -94,10 +97,14 @@ class LinebotController < ApplicationController
           text: push
         }
         client.reply_message(event['replyToken'], message)
+      # LINEお友達追された場合
       when Line::Bot::Event::Follow
+        # 登録したユーザーのidをユーザーテーブルに格納
         line_id = event['source']['userId']
         User.create(line_id: line_id)
+      # LINEお友達解除された場合
       when Line::Bot::Event::Unfollow
+        # お友達解除したユーザーのデータをユーザーテーブルから削除
         line_id = event['source']['userId']
         User.find_by(line_id: line_id).destroy
       end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -10,16 +10,19 @@ task :update_feed => :environment do
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
 
-  url  = "http://www.drk7.jp/weather/xml/13.xml"
+  # 使用したxmlデータ（毎日朝6時更新）：URL入力すれば見れます
+  url  = "https://www.drk7.jp/weather/xml/13.xml"
+  # xmlデータをパース（利用しやすいように整形）
   xml  = open( url ).read.toutf8
   doc = REXML::Document.new(xml)
-  xpath = 'weatherforecast/pref/area[4]/info'
-  weather = doc.elements[xpath + '/weather'].text
-  per06to12 = doc.elements[xpath + '/rainfallchance/period[2]l'].text
-  per12to18 = doc.elements[xpath + '/rainfallchance/period[3]l'].text
-  per18to24 = doc.elements[xpath + '/rainfallchance/period[4]l'].text
+  # パスの共通部分を変数化（area[4]は「東京地方」：URLを入力して見れば分かります）
+  xpath = 'weatherforecast/pref/area[4]/info/rainfallchance/'
+  # 6時〜12時の降水確率（以下同様）
+  per06to12 = doc.elements[xpath + 'period[2]'].text
+  per12to18 = doc.elements[xpath + 'period[3]'].text
+  per18to24 = doc.elements[xpath + 'period[4]'].text
+  # メッセージを発信する降水確率の下限値の設定
   min_per = 20
-  mid_per = 50
   if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
     word1 =
       ["いい朝だね！",
@@ -33,13 +36,17 @@ task :update_feed => :environment do
        "雨に負けずに今日も頑張ってね(^^)",
        "今日も一日楽しんでいこうね(^^)",
        "楽しいことがありますように(^^)"].sample
+    # 降水確率によってメッセージを変更する閾値の設定
+    mid_per = 50
     if per06to12.to_i >= mid_per || per12to18.to_i >= mid_per || per18to24.to_i >= mid_per
       word3 = "今日は雨が降りそうだから傘を忘れないでね！"
     else
       word3 = "今日は雨が降るかもしれないから折りたたみ傘があると安心だよ！"
     end
+    # 発信するメッセージの設定
     push =
       "#{word1}\n#{word3}\n降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word2}"
+    # メッセージの発信先idを配列で渡す必要があるため、userテーブルよりpluck関数を使ってidを配列で取得
     user_ids = User.all.pluck(:line_id)
     message = {
       type: 'text',
