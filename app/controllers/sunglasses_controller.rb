0f27f7ad13b9c86ae92a483a294a02c09858325e
Yusuke Miyazaki <ysk1180@gmail.com>
2018-07-15 16:23:20 +0900

    コントローラにbody取得の一文追加

--- app/controllers/sunglasses_controller.rb
+++ app/controllers/sunglasses_controller.rb
@@ -5,6 +5,7 @@ class SunglassesController < ApplicationController
   protect_from_forgery :except => [:callback]
 
   def callback
+    body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
