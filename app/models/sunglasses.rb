826b5be3566cadddb0e90ac3cca6ad37c51af721
Yusuke Miyazaki <ysk1180@gmail.com>
2018-07-15 15:34:42 +0900

    [change]リファクタ、メッセージの定数追加

--- app/models/sunglasses.rb
+++ app/models/sunglasses.rb
@@ -2,10 +2,14 @@ class Sunglasses
   require 'line/bot'
   def self.sunglasses_main
     # 本日が休日（土日・祝日）に当たるかの取得
-    holiday_or_not = Holiday.holiday(Date.today)
-    puts holiday_or_not
-    # 本日の天気の取得
-    weather = Weather.weather
+    holiday_or_not = Holiday.holiday(Time.zone.today)
+    if holiday_or_not
+      # 本日の天気の取得
+      weather = Weather.weather
+      if weather =~ /.*晴れ.*/
+        word = Settings.word.warning
+      end
+    end
   end
   def client
     client ||= Line::Bot::Client.new { |config|
