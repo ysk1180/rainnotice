73fb9c4a25a6ad4e1146a7306a2939019dfbe53a
Yusuke Miyazaki <ysk1180@gmail.com>
2018-07-15 16:04:54 +0900

    サングラスモデルとコントローラの配置修正

--- app/controllers/sunglasses_controller.rb
+++ app/controllers/sunglasses_controller.rb
@@ -1,9 +1,14 @@
 class SunglassesController < ApplicationController
+  require 'line/bot'
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def callback
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
     Sunglasses.friend
   end
 end
--- app/models/sunglasses.rb
+++ app/models/sunglasses.rb
@@ -14,10 +14,6 @@ class Sunglasses
   end
 
   def self.friend
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
     events = client.parse_events_from(body)
     events.each { |event|
       case event
