a6edb19cc9dcc40904dd5991d52cc41c542fdb18
Yusuke Miyazaki <ysk1180@gmail.com>
2018-07-15 14:23:42 +0900

    [add]()を消去
    rubocopの指摘点対応

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -4,14 +4,17 @@ class Holiday
   def self.holiday(day)
     weekend(day) || public_holiday(day) || substitute_holiday(day)
   end
+
   def self.weekend(day)
-    !(day.workday?)
+    !day.workday?
   end
+
   def self.public_holiday(day)
-    !!(HolidayJapan.check(day))
+    !!HolidayJapan.check(day)
   end
+
   # 振替休日
   def self.substitute_holiday(day)
-    day.wday == 1 && !!(HolidayJapan.check(day))
+    day.wday == 1 && !!HolidayJapan.check(day)
   end
 end
