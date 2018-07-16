cdf10b0c0606fb4a8a68329a95fb0b54e3f5d6d8
Yusuke Miyazaki <ysk1180@gmail.com>
2018-07-16 19:17:20 +0900

    プッシュメッセージ機能追加3

--- app/models/sunglasses.rb
+++ app/models/sunglasses.rb
@@ -9,19 +9,19 @@ class Sunglasses
       weather = Weather.weather
       if weather =~ /.*晴れ.*/
         word = Settings.word.warning
+        client ||= Line::Bot::Client.new { |config|
+          config.channel_secret = ENV["LINE_CHANNEL_SECRET_SUN"]
+          config.channel_token = ENV["LINE_CHANNEL_TOKEN_SUN"]
+        }
+        # メッセージの発信先idを配列で渡す必要があるため、userテーブルよりpluck関数を使ってidを配列で取得
+        user_ids = SunUser.all.pluck(:line_id)
+        message = {
+          type: 'text',
+          text: word
+        }
+        response = client.multicast(user_ids, message)
+        "OK"
       end
-      client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET_SUN"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN_SUN"]
-      }
-      # メッセージの発信先idを配列で渡す必要があるため、userテーブルよりpluck関数を使ってidを配列で取得
-      user_ids = SunUser.all.pluck(:line_id)
-      message = {
-        type: 'text',
-        text: word
-      }
-      response = client.multicast(user_ids, message)
-      "OK"
     end
   end
 end
